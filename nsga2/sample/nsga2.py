36348e9d73199ae120ff926c9ceb6a3d2dab22ec
ThalesORP <ThalesORP@gmail.com>
2019-08-25 01:47:41 -0300

    First attempt of the Crowding Distance sorting.

--- nsga2/sample/nsga2.py
+++ nsga2/sample/nsga2.py
@@ -15,28 +15,37 @@
 '''Main class of NSGA-II.'''
 import sys
 import random
+
 from .individual import Individual
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 class NSGA2:
     '''Main class of the algorithm.'''
 
     # Attributes
-    POPULATION_SIZE = 5
+    POPULATION_SIZE = 10
     OFFSPRING_SIZE = 5 # A prole, descendentes.
 
     X_MIN_VALUE = 0
-    X_MAX_VALUE = 5
+    X_MAX_VALUE = 10
 
     Y_MIN_VALUE = 0
-    Y_MAX_VALUE = 5
+    Y_MAX_VALUE = 10
 
     # Constructor
     def __init__(self):
-        random.seed(1)
+        random.seed(3)
+
         self.population = None
+        self.offspring = None
+
+        self.x_values = list()
+        self.y_values = list()
+
         # List with all fronts.
         self.fronts = list()
-        self.fronts.append([])
 
     # Methods
     def run(self):
@@ -51,31 +60,42 @@ class NSGA2:
         Back to beginning. Repeated N generations.
         ''' # pylint: disable=pointless-string-statement
 
-        #self.start_new_population()
-        self.start_new_population_debug()
+        self.start_new_population()
+        #self.start_new_population_debug()
 
         self.non_dominated_sorting()
+
+        self.crowding_distance_sorting()
+
+        self._show_offspring()
+
+        #self._plot_individuals()
+        self._plot_individuals_fronts()
 
     def start_new_population(self):
         '''Initialize a new population.'''
 
-        names = 'ABCDEFGHIJKL'
+        names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         self.population = list()
 
         for i in range(self.POPULATION_SIZE):
             x_value = random.randint(self.X_MIN_VALUE, self.X_MAX_VALUE)
+            self.x_values.append(x_value)
             y_value = random.randint(self.Y_MIN_VALUE, self.Y_MAX_VALUE)
+            self.y_values.append(y_value)
             individual = Individual(names[i], x_value, y_value)
             self.population.append(individual)
 
     def non_dominated_sorting(self):
-        '''Non-dominated sorting algorithm.'''
+        '''Sort the individuals according to they dominance and sort them into fronts.'''
 
         ''' Everyone check with everyone who dominates who, filling up
         "domination_count" and "dominated_by" attributes of each individual.
         Also, the first front is created.
         Then the remaining individuals are divided into fronts.
         ''' # pylint: disable=pointless-string-statement
+
+        self.fronts.append([])
 
         # Each of individuals checks if dominates or is dominated with everyone else.
         for i in range(self.POPULATION_SIZE):
@@ -125,6 +145,74 @@ class NSGA2:
         self._show_general_domination_info()
         self._show_fronts()
 
+    def crowding_distance_sorting(self):
+        '''Crowding distance sorting algorithm.'''
+
+        self.offspring = list()
+        individual_remaining = self.OFFSPRING_SIZE
+
+        # Filling up the offspring with the first fronts.
+        for front in self.fronts:
+            front_size = len(front)
+            # Checking if the current front fits on offspring.
+            if front_size <= individual_remaining:
+                # Putting one by one on offspring.
+                for individual in front:
+                    self.offspring.append(individual)
+                    individual_remaining -= 1
+            else:
+                # The current front is stored.
+                remaining_front = front
+                break
+
+        self.sort_individuals(remaining_front)
+
+        # Creating a list of index that alternates with the lowest and highest values.
+        index_list = []
+        full_value = len(remaining_front)-1
+        full_value = 5-1
+        for i in range(int(full_value/2)):
+            index_list.append(i)
+            index_list.append(full_value)
+            full_value -= 1
+        index_list.append(i+1)
+        if full_value % 2 == 1:
+            index_list.append(full_value)
+
+        # Adding the last but not least individuals selected.
+        i = 0
+        while individual_remaining > 0:
+            self.offspring.append(remaining_front[index_list[i]])
+            individual_remaining -= 1
+            i += 1
+
+    # Utils
+    def sort_individuals(self, individual_list):
+        '''Sort an list of individuals.'''
+
+        sum_list = list()
+        xy_sum = 0
+        for individual in individual_list:
+            xy_sum = individual.x_value + individual.y_value
+            sum_list.append(xy_sum)
+
+        lowest = None
+        for i in range(len(individual_list)):
+            # Finding the lowest value.
+            lowest_index = 0
+            lowest = sum_list[0]
+            for j in range(1, len(sum_list)):
+                if sum_list[j] < lowest:
+                    lowest_index = j
+                    lowest = sum_list[j]
+
+            # Remove an element from list by index.
+            sum_list.pop(lowest_index)
+            lowest = individual_list.pop(lowest_index)
+            # Insert the lowest to the first available position.
+            sum_list.insert(0, sys.maxsize)
+            individual_list.insert(i, lowest)
+
     # Debug
     def start_new_population_debug(self):
         '''DEBUG: Initialize a new population.'''
@@ -145,8 +233,8 @@ class NSGA2:
         individual = Individual('E', 2, 1)
         self.population.append(individual)
 
-        #individual = Individual('F', 3, 2)
-        #self.population.append(individual)
+        individual = Individual('F', 3, 2)
+        self.population.append(individual)
 
     def _show_population(self):
         '''Show the x and y values of each individual of population.'''
@@ -174,3 +262,62 @@ class NSGA2:
             for individual in front:
                 sys.stdout.write(str(individual) + ", ")
             print("")
+
+    def _show_offspring(self):
+        '''Show individuals of the offspring.'''
+        print("\nOffspring:")
+        for individual in self.offspring:
+            sys.stdout.write(str(individual) + ", ")
+        print("")
+
+    def _show_individuals_from_list(self, individual_list):
+        for individual in individual_list:
+            sys.stdout.write(str(individual) + ', ')
+        print()
+
+    def _plot_individuals(self):
+        plt.plot(self.x_values, self.y_values, 'ko')
+        plt.axis([self.X_MIN_VALUE, self.X_MAX_VALUE, self.Y_MIN_VALUE, self.Y_MAX_VALUE])
+        plt.xticks(np.arange(self.X_MIN_VALUE, self.X_MAX_VALUE+1, 1.0))
+        plt.yticks(np.arange(self.Y_MIN_VALUE, self.Y_MAX_VALUE+1, 1.0))
+        plt.show()
+
+    def _plot_individuals_fronts(self):
+        multiple_x_values = list()
+        multiple_y_values = list()
+        x_values = list()
+        y_values = list()
+
+        for front in self.fronts:
+            x_values = list()
+            y_values = list()
+            for individual in front:
+                x_values.append(individual.x_value)
+                y_values.append(individual.y_value)
+            multiple_x_values.append(x_values)
+            multiple_y_values.append(y_values)
+
+        #colors = ['go','ro','bo', 'mo', 'yo', 'co', 'no', 'ko']
+        colors = ['bo', 'go', 'ro', 'co', 'mo', 'yo', 'ko', 'wo']
+        labels = ['Front 1', 'Front 2', 'Front 3', 'Front 4', 'Front 5', 'Front 6', 'Front 7', 'Front 8']
+
+        ax = plt.subplot(111)
+
+        i = 0
+        for i in range(len(multiple_x_values)):
+            plt.plot(multiple_x_values[i], multiple_y_values[i], colors[i], label=labels[i])
+
+        #plt.plot(self.x_values, self.y_values, 'ro')
+        #plt.plot(self.x_values, self.y_values, 'ro')
+
+        plt.axis([self.X_MIN_VALUE, self.X_MAX_VALUE, self.Y_MIN_VALUE, self.Y_MAX_VALUE])
+        plt.xticks(np.arange(self.X_MIN_VALUE, self.X_MAX_VALUE+1, 1.0))
+        plt.yticks(np.arange(self.Y_MIN_VALUE, self.Y_MAX_VALUE+1, 1.0))
+
+        plt.title('Individual fronts')
+        chartBox = ax.get_position()
+        ax.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.6, chartBox.height])
+        ax.legend(loc='upper center', bbox_to_anchor=(1.45, 0.8), shadow=True, ncol=1)
+
+        #plt.legend(loc='upper left')
+        plt.show()
